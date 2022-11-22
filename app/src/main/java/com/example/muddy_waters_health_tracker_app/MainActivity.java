03cc14b81ac83f36fcfc88e8542bfdb04a147140
chonger878 <echong06@gmail.com>
2022-11-22 01:40:12 -0800

    added new Intent

--- app/src/main/java/com/example/muddy_waters_health_tracker_app/MainActivity.java
+++ app/src/main/java/com/example/muddy_waters_health_tracker_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.muddy_waters_health_tracker_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(getBaseContext(),NewUserActivity.class);
 
     }
 
